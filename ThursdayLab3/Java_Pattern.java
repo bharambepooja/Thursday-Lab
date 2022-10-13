c5efcfe84c03403eb03513d5c3a4a0bb29346848
bharambepooja <114000660+bharambepooja@users.noreply.github.com>
2022-10-13 22:45:38 +0530

    Create Java_Pattern.java

---
+++ ThursdayLab3/Java_Pattern.java
@@ -0,0 +1,18 @@
+/*
+Q5.
+Write a Java program to display the following pattern.
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+ */
+
+public class Java_Pattern {
+
+	public static void main(String[] args) {
+
+		System.out.println("    J     a    v     v    a");
+		System.out.println("    J    a a    v   v    a a");
+		System.out.println("J   J   aaaaa    v v    aaaaa");
+		System.out.println(" J J   a     a    v    a     a");
+	}
